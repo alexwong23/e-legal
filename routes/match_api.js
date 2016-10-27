a668c39be0ec7bdd1b443bf0f16374f9f903fdde
Alex Wong <alexwongweilun@hotmail.co.uk>
2016-10-27 13:20:19 +0800

    anything

--- routes/match_api.js
+++ routes/match_api.js
@@ -6,8 +6,8 @@ var Match = require('../models/match')
 var Vote = require('../models/vote')
 
 // time now is 27 oct 12 20 00
-var matchDate = '2016-10-27 15:20:30' // simulate cancel vote 3 hr b4
-var matchDate2 = '2016-10-27 12:20:45' // simulate game starting
+var matchDate = '2016-10-27 15:21:30' // simulate cancel vote 3 hr b4
+var matchDate2 = '2016-10-27 13:21:45' // simulate game starting
 
 // initial
 // assume 28 oct 1659, first game stop predict, show countdown
